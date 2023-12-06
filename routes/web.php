4eb56ae1a7e4ce238f1393d2e3be85237ba9316c
Eric Muga <eric.muga@gmail.com>
2023-12-06 07:42:07 +0300

    required auth.php at the top

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\ItemController;
-
+require __DIR__.'/auth.php';
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -86,4 +86,4 @@ Route::middleware('auth')->group(function () {
 
 });
 
-require __DIR__.'/auth.php';
+
