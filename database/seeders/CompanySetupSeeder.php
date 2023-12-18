3c64d6af01d4f705204fb12b10758cc5e165a6b5
Eric Muga <eric.muga@gmail.com>
2023-12-18 14:00:14 +0300

    added company seeder

--- database/seeders/CompanySetupSeeder.php
+++ database/seeders/CompanySetupSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\CompanySetup;
 
 class CompanySetupSeeder extends Seeder
 {
@@ -12,6 +13,16 @@ class CompanySetupSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        CompanySetup::create([
+          'email'=>'batian@gmail.com',
+          'company_name'=>'Batian Optical Centre',
+          'postal_address'=>'2066-10100 Nyeri',
+          'pin'=>'P00012834J',
+          'bank_name'=>'Coop',
+          'account_no'=>'011000003242',
+          'branch_name'=>'Nyeri'
+
+        ]);
+
     }
 }
