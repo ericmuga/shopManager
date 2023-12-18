02449950ae78e7b375930ce251d13ae0bad26898
Eric Muga <eric.muga@gmail.com>
2023-12-18 05:00:05 +0300

    changed pdf import

--- resources/js/Pages/Sales/OrderList.vue
+++ resources/js/Pages/Sales/OrderList.vue
@@ -15,12 +15,24 @@ import axios from 'axios';
 import moment from 'moment';
 // import logo from '@/assets/logo.jpg';
 
-import pdfMake from 'pdfmake/build/pdfmake';
-// import pdfFonts from 'pdfmake/build/vfs_fonts';
-import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+// import pdfMake from 'pdfmake/build/pdfmake';
+// // import pdfFonts from 'pdfmake/build/vfs_fonts';
+// import {fonts} from 'pdfmake/build/vfs_fonts';
 
 
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
+// pdfMake.vfs = fonts;
+
+import "pdfmake/build/pdfmake";
+const pdfMake = window["pdfMake"];
+
+pdfMake.fonts = {
+    Roboto: {
+        normal: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Regular.ttf",
+        bold: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Medium.ttf",
+        italics: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Italic.ttf",
+        bolditalics: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-MediumItalic.ttf",
+    },
+};
 
 
 const totalQuantity = computed(() => {
